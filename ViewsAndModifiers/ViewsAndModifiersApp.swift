d211ac15a8562fde8c6e9fd9c078e0ea9044cbe5
Rachelle Lee <rachedeveloper@gmail.com>
2021-07-17 16:30:43 -0700

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by RachelleLee on 7/17/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
