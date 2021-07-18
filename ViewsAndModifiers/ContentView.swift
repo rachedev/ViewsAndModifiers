62ed54c02d1fcffc2b3556d608c4888c8e88cfb2
Rachelle Lee <rachedeveloper@gmail.com>
2021-07-18 16:03:44 -0700

    After Challenge

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,10 +7,51 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+            .padding()
+            .background(Color.white)
+//            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+//struct Watermark: ViewModifier {
+//    var text: String
+//
+//    func body(content: Content) -> some View {
+//        ZStack(alignment: .bottomTrailing) {
+//            content
+//            Text(text)
+//                .font(.caption)
+//                .foregroundColor(.white)
+//                .padding(5)
+//                .background(Color.black)
+//        }
+//    }
+//}
+
+//extension View {
+//    func watermarked(with text: String) -> some View {
+//        self.modifier(Watermark(text: text))
+//    }
+//}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Some Prominent Title")
+            .modifier(Title())
+//        Color.blue
+//            .frame(width: 300, height: 200)
+//            .watermarked(with: "Hacking with Swift")
     }
 }
 
